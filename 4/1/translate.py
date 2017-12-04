5dc5f9876192e4f3b08e928dc45f7b8a82e3bf20
Lorenz Leutgeb <lorenz.leutgeb@sclable.com>
2017-12-04 11:20:08 +0100

    Day 4: High-Entropy Passphrases

---
+++ 4/1/translate.py
@@ -0,0 +1,6 @@
+import sys
+
+for i, row in enumerate(sys.stdin):
+    print('row({}).'.format(i))
+    for j, word in enumerate(row.split(' ')):
+        print('word({},{},"{}").'.format(i, j, word.strip()))
