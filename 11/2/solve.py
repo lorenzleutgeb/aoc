8bf97b57e6a069f948876deadb44fe2ea43790c7
Lorenz Leutgeb <lorenz.leutgeb@sclable.com>
2017-12-11 06:36:57 +0100

    Day 11: Hex Ed

---
+++ 11/2/solve.py
@@ -0,0 +1,25 @@
+import sys
+
+x, y = 0, 0
+
+for step in sys.stdin.readline().strip().split(','):
+    if step == 'n':
+        y = y + 1
+    if step == 'ne':
+        y = y + 0.5
+        x = x + 0.5
+    if step == 'nw':
+        y = y + 0.5
+        x = x - 0.5
+    if step == 's':
+        y = y - 1
+    if step == 'sw':
+        y = y - 0.5
+        x = x - 0.5
+    if step == 'se':
+        y = y - 0.5
+        x = x + 0.5
+
+    d = max(d, abs(x) + abs(y))
+
+print(d)
