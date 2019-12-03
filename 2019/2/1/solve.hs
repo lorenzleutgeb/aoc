535f088d04a07caf2d20e58689b26f73a4e25054
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-12-03 11:11:38 +0100

    Day 2: 1202 Program Alarm

---
+++ 2019/2/1/solve.hs
@@ -0,0 +1,17 @@
+import Data.List.Split
+import Data.Vector
+
+op :: (Int -> Int -> Int) -> Int -> Vector Int -> Vector Int
+op f ptr code = update code $ V.fromList [(code!(ptr + 3), (code!(code!(ptr + 1))) `f` (code!(code!(ptr + 2))))]
+
+run :: Int -> Vector Int -> Int
+run ptr code = case code!ptr of
+  1 -> run (ptr + 4) $ op (+) ptr code
+  2 -> run (ptr + 4) $ op (*) ptr code
+  99 -> code!0
+  _ -> error "Unknown opcode"
+
+restore :: Vector Int -> Vector Int
+restore code = update code (fromList [(1, 12), (2, 2)])
+
+main = interact (show . run 0 . restore . fromList . fmap read . splitOn ",")
---
+++ 2019/2/2/solve.hs
@@ -0,0 +1,27 @@
+import Data.List       as L
+import Data.List.Split as S
+import Data.Maybe      as M
+import Data.Vector     as V
+
+op :: (Int -> Int -> Int) -> Int -> Vector Int -> Vector Int
+op f ptr code = update code $ V.fromList [(code!(ptr + 3), (code!(code!(ptr + 1))) `f` (code!(code!(ptr + 2))))]
+
+run :: Int -> Vector Int -> Int
+run ptr code = case code!ptr of
+  1 -> run (ptr + 4) $ op (+) ptr code
+  2 -> run (ptr + 4) $ op (*) ptr code
+  99 -> code!0
+  _ -> error "Unknown opcode"
+
+restore :: Vector Int -> (Int, Int) -> Vector Int
+restore code (noun, verb) = update code $ V.fromList [(1, noun), (2, verb)]
+
+combine (noun, verb) = 100 * noun + verb
+
+goal :: Vector Int -> (Int, Int) -> Bool
+goal code (noun, verb) = (==) 19690720 $ run 0 $ restore code (noun, verb)
+
+match :: Vector Int -> (Int, Int)
+match code = M.fromJust $ L.find (goal code) [(noun, verb) | noun <- [0..99], verb <- [0..99]]
+
+main = interact $ show . combine . match . fromList . fmap read . S.splitOn ","
