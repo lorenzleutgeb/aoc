af1ea9600d7b60877558cd049620769aa27a6fc7
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-12-04 00:03:54 +0100

    Day 3: Crossed Wires

---
+++ 2019/3/1/solve.hs
@@ -0,0 +1,33 @@
+import Data.List       as L
+import Data.List.Split as LS
+import Data.Set        as S
+
+type Point = (Int, Int)
+
+origin :: Point
+origin = (0, 0)
+
+manhattan (x1, y1) (x2, y2) = (abs $ x1 - x2) + (abs $ y1 - y2)
+
+data Direction = U | D | L | R deriving (Read, Show)
+
+neighbor (x, y) R = (x + 1, y)
+neighbor (x, y) L = (x - 1, y)
+neighbor (x, y) U = (x, y + 1)
+neighbor (x, y) D = (x, y - 1)
+
+type Movement = (Direction, Int)
+
+parse :: String -> Movement
+parse (x:xs) = (read [x], read xs)
+
+explode _ []          = S.empty
+explode c ((_, 0):ms) = explode c ms
+explode c ((d, l):ms) = S.insert c (explode (neighbor c d) ((d, l - 1):ms))
+
+crossings [a, b] = toList $ S.delete origin $ intersection a b
+
+main = interact $ show . minimum
+  . L.map (manhattan origin) . crossings
+  . L.map (explode origin . (\x -> L.map parse (LS.splitOn "," x)))
+  . lines
---
+++ 2019/3/2/solve.hs
@@ -0,0 +1,41 @@
+import Data.List       as L
+import Data.List.Split as LS
+import Data.Set        as S
+import Data.Map        as M
+import Data.Maybe      as Mb
+
+type Point = (Int, Int)
+
+origin :: Point
+origin = (0, 0)
+
+neighbor (x, y) R = (x + 1, y)
+neighbor (x, y) L = (x - 1, y)
+neighbor (x, y) U = (x, y + 1)
+neighbor (x, y) D = (x, y - 1)
+
+data Direction = U | D | L | R deriving (Read, Show)
+
+type Movement = (Direction, Int)
+
+parse :: String -> Movement
+parse (x:xs) = (read [x], read xs)
+
+alterIfSmaller x Nothing  = Just x
+alterIfSmaller x (Just y) = Just (min x y)
+
+explode s ps ss _ []          = (ps, ss)
+explode s ps ss c ((_, 0):ms) = explode s ps ss c ms
+explode s ps ss c ((d, l):ms) = (S.insert c ps', M.alter (alterIfSmaller s) c ss')
+  where (ps', ss') = (explode (s + 1) ps ss (neighbor c d) ((d, l - 1):ms))
+
+addLookups a b k = (Mb.fromJust $ M.lookup k a) + (Mb.fromJust $ M.lookup k b)
+
+crossings :: [(Set Point, Map Point Int)] -> [Int]
+crossings [(aps, ass), (bps, bss)] = L.map (addLookups ass bss) $ S.toList $ S.delete origin (S.intersection aps bps)
+
+main = interact $ show
+  . minimum
+  . crossings
+  . L.map (explode 0 S.empty M.empty origin . (\x -> L.map parse (LS.splitOn "," x)))
+  . lines
