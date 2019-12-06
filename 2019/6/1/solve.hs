97f4a94edc2705efec877d7cded3d1c54b8554f4
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-12-06 08:10:03 +0100

    Day 6: Universal Orbit Map

---
+++ 2019/6/1/solve.hs
@@ -0,0 +1,8 @@
+import           Data.List.Split       (splitOn)
+import qualified Data.MultiMap   as MM (fromList, lookup)
+
+ltop [x, y] = (x, y)
+
+orbits n a mm = n + sum (map (\b -> orbits (n + 1) b mm) $ MM.lookup a mm)
+
+main = interact $ show . orbits 0 "COM" . MM.fromList . map (ltop . splitOn ")") . lines
---
+++ 2019/6/2/solve.hs
@@ -0,0 +1,16 @@
+import           Data.List.Split      (splitOn)
+import qualified Data.Map        as M (fromList, lookup)
+import           Data.Maybe           (fromJust)
+
+ltop [x, y] = (x, y)
+
+pathTo "COM" m = []
+pathTo a     m = a : pathTo (fromJust $ M.lookup a m) m
+
+dropWhilePrefix (x:as) (y:bs) | x == y    = dropWhilePrefix as bs
+                              | otherwise = (as, bs)
+
+distance m = length a + length b
+  where (a, b) = dropWhilePrefix (reverse $ pathTo "SAN" m) (reverse $ pathTo "YOU" m)
+
+main = interact $ show . distance . M.fromList . map (ltop . reverse . splitOn ")") . lines
