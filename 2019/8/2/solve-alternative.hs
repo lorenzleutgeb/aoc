2f0e93944824d5e466b001a45813c7d47cf4dc9e
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-12-08 07:54:35 +0100

    Day 8: Space Image Format

---
+++ 2019/8/2/solve-alternative.hs
@@ -0,0 +1,24 @@
+import Data.Char       (digitToInt)
+import Data.List       (intercalate, transpose)
+import Data.List.Split (chunksOf)
+
+width  = 25
+height =  6
+
+data Color = Black | White | None deriving (Enum)
+
+instance Semigroup Color where
+  None <> x = x
+  x    <> _ = x
+
+instance Monoid Color where
+  mempty = None
+
+instance Show Color where
+  show White = "\x2588"
+  show _     = " "
+
+main = interact
+  $ intercalate "\n" . map (map (head . show)) . chunksOf width
+  . map mconcat . transpose . chunksOf (width * height)
+  . map ((toEnum :: Int -> Color) . digitToInt) . head . lines
---
+++ 2019/8/2/solve.hs
@@ -0,0 +1,20 @@
+import Data.Char       (digitToInt)
+import Data.List       (intercalate)
+import Data.List.Split (chunksOf)
+
+width  = 25
+height =  6
+
+data Color = Black | White | None deriving (Enum)
+
+instance Show Color where
+  show White = "\x2588"
+  show _     = " "
+
+merge None x = x
+merge x    _ = x
+
+main = interact
+  $ intercalate "\n" . map (map (head . show)) . chunksOf width
+  . foldl1 (zipWith merge)
+  . chunksOf (width * height) . map (toEnum . digitToInt) . head . lines
