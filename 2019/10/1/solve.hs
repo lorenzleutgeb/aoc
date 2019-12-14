629777b8ffaf0b9dcb474598c8e697c681277b33
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-12-14 16:03:34 +0100

    Day 10: Monitoring Station

---
+++ 2019/10/1/solve.hs
@@ -0,0 +1,9 @@
+import Data.List (nub)
+
+main = interact
+  $ show . maximum
+  . map (length . nub . map ((\(x, y) -> let z = gcd x y in (div x z, div y z))))
+  . (\as -> (\a -> [(fst b - fst a, snd b - snd a) | b <- as, b /= a]) <$> as)
+  . (\input -> [(x, y) | (y, ln) <- index $ lines input, (x, '#') <- index ln])
+  where
+    index = zip [0..]
