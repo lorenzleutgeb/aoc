9b55b2e600eab3a1b4c7addee32fcdc0fed96264
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2020-12-02 08:52:34 +0100

    Day 2: Password Philosophy

---
+++ 2020/2/1/solve.hs
@@ -0,0 +1,42 @@
+{- stack script
+ --resolver lts-16.24
+ --package parsec
+ --ghc-options -Wall
+ -}
+
+import Text.Parsec
+import Text.Parsec.String
+
+main :: IO ()
+main =
+  interact $
+    show
+      . length
+      . filter check
+      . eitherError
+      . parse (many line) ""
+
+eitherError :: Show a => Either a b -> b
+eitherError = either (error . show) id
+
+type Input = ((Int, Int), Char, String)
+
+check :: Input -> Bool
+check (range, c, password) = let cnt = (length $ filter (c ==) password) in (fst range) <= cnt && cnt <= (snd range)
+
+line :: Parser Input
+line = do
+  rangeMin <- many1 digit
+  _ <- char '-'
+  rangeMax <- many1 digit
+  _ <- char ' '
+  c <- letter
+  _ <- string ": "
+  password <- rest
+  return $ ((read rangeMin, read rangeMax), c, password)
+
+lf :: Char
+lf = '\n'
+
+rest :: Parser String
+rest = many1 (noneOf [lf]) <* char lf
