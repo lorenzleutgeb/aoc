45d670d7cfd385b5ca072196ddf6a1652e7aa97b
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2020-12-25 08:53:46 +0100

    Day 25: Combo Breaker

---
+++ 2020/25/solve.hs
@@ -0,0 +1,20 @@
+{- stack script
+ --resolver lts-16.24
+ --ghc-options -Wall
+ --package arithmoi
+ -}
+
+import Math.NumberTheory.Powers.Modular (powMod)
+
+m :: Int
+m = 20201227
+
+card :: Int
+card = 9232416
+
+door :: Int
+door = 14144084
+
+main :: IO ()
+main = do
+  print $ powMod card (head [i | i <- [3..], powMod 7 i m == door]) m
